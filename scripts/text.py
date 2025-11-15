59e3f3a178f574ae4640cf992976e9567769667b
CmdrApollo <henryj2545@gmail.com>
2025-11-15 15:39:24 -0600

    fixed text

--- scripts/text.py
+++ scripts/text.py
@@ -2,14 +2,14 @@ from .constants import *
 
 game_text_en = {
     "TITLE_WELCOME": "Welcome",
-    "BODY_WELCOME": f"    Welcome to {gamename}, the all-in-one island colony sim! If this is your first time playing, it is recommended that you check out the tutorial!\n    {gamename} is and always will be 100% free and open-source. If you wish to contribute to {gamename}, head to the github page and become a contributor!",
+    "BODY_WELCOME": f"    Welcome to {gamename}, the all-in-one island grand strategy game! If this is your first time playing, it is recommended that you check out the tutorial!\n    {gamename} is and always will be 100% free and open-source. If you wish to contribute to {gamename}, head to the github page and become a contributor!",
     "TITLE_TUTORIAL": "Tutorial",
     "BODY_TUTORIAL": f"    Welcome to the {gamename} tutorial! To get started, left-click on a tile in the world to view its stats!",
 }
 
 game_text_es = {
     "TITLE_WELCOME": "Bienvenidos",
-    "BODY_WELCOME": f"    Bienvenidos a {gamename}, el todo-en-uno simulador de colonias con islas! Si es tu primera vez jugando, ¡es recomendado que vayas al tutorial!\n    {gamename} es y siempre será 100% gratis y tendrá código abierto. Si quisieras contribuir a {gamename}, ¡vé a la página github y conviértete en colaborador(a)!",
+    "BODY_WELCOME": f"    Bienvenidos a {gamename}, el todo-en-uno juego de gran estrategía con islas! Si es tu primera vez jugando, ¡es recomendado que vayas al tutorial!\n    {gamename} es y siempre será 100% gratis y tendrá código abierto. Si quisieras contribuir a {gamename}, ¡vé a la página github y conviértete en colaborador(a)!",
     "TITLE_TUTORIAL": "Tutorial",
     "BODY_TUTORIAL": f"    ¡Bienvenidos al tutorial de {gamename}! Para empezar, ¡haz clic izquierda por una pieza en el mundo para ver su estado!",
 }
